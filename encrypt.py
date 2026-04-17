3d995755fe92c331dc3ab1212a47c3f0e42eccf0
Elita Jashari <elita.jashari@student.uni-pr.edu>
2026-04-17 13:52:12 +0200

    Book cipher encryption
    Kodi fillestar për enkriptimin e Book-cipher

---
+++ encrypt.py
@@ -0,0 +1,37 @@
+def clean_text(text):
+    return text.lower().replace("\n", " ").split()
+
+
+def build_book_index(book_text):
+    words = clean_text(book_text)
+    index_map = {}
+
+    for i, word in enumerate(words, start=1):
+        if word not in index_map:
+            index_map[word] = []
+        index_map[word].append(i)
+
+    return index_map
+
+
+def book_cipher_encrypt(message, book_text):
+    index_map = build_book_index(book_text)
+    message_words = clean_text(message)
+
+    encrypted = []
+
+    for word in message_words:
+        if word in index_map:
+            encrypted.append(str(index_map[word][0]))
+        else:
+            encrypted.append("?")
+
+    return " ".join(encrypted)
+
+
+if __name__ == "__main__":
+    book = input("Shkruaj tekstin e librit:\n")
+    message = input("Shkruaj mesazhin:\n")
+
+    encrypted = book_cipher_encrypt(message, book)
+    print("Encrypted:", encrypted)
