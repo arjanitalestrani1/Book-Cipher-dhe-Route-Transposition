d6777e67ae3a24b0b3eac312147462e4bf6cd545
Elita Jashari <elita.jashari@student.uni-pr.edu>
2026-04-18 20:42:39 +0200

    book_cipher_encrypt
    Book cipher encrypt, file i permisuar.

---
+++ main.py
@@ -0,0 +1,9 @@
+# BOOK CIPHER ENCRYPT SHEMBULLI
+
+
+#if __name__ == "__main__":
+#   book = input("Shkruaj tekstin e librit:\n")
+#   message = input("Shkruaj mesazhin:\n")
+
+#   encrypted = book_cipher_encrypt(message, book)
+#   print("Encrypted:", encrypted)
