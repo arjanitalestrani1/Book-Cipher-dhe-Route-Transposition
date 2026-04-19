824b4ca61f7589c004f80769e3d3cff3d5b05268
EliraBytyqi <elira.bytyqi@student.uni-pr.edu>
2026-04-19 16:20:47 +0200

    Add main menu and integrate route & book cipher functions

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
-# BOOK CIPHER ENCRYPT SHEMBULLI
+from route_transposition_cipher import (
+    encrypt_column_route,
+    encrypt_zigzag_route,
+    decrypt_column_route,
+    decrypt_zigzag_route
+)
 
+from book_cipher import (
+    book_cipher_encrypt,
+    book_cipher_decrypt
+)
 
-#if __name__ == "__main__":
-#   book = input("Shkruaj tekstin e librit:\n")
-#   message = input("Shkruaj mesazhin:\n")
+print("Choose option:")
+print("1. Column Route Encrypt")
+print("2. Column Route Decrypt")
+print("3. ZigZag Route Encrypt")
+print("4. ZigZag Route Decrypt")
+print("5. Book Cipher Encrypt")
+print("6. Book Cipher Decrypt")
 
-#   encrypted = book_cipher_encrypt(message, book)
-#   print("Encrypted:", encrypted)
+choice = input("Enter choice: ")
+text = input("Enter text: ")
+
+if choice in ["1", "2", "3", "4"]:
+    cols = int(input("Enter columns: "))
+
+if choice in ["5", "6"]:
+    book_text = input("Enter book text: ")
+
+if choice == "1":
+    print("Encrypted:", encrypt_column_route(text, cols))
+
+elif choice == "2":
+    print("Decrypted:", decrypt_column_route(text, cols))
+
+elif choice == "3":
+    print("Encrypted:", encrypt_zigzag_route(text, cols))
+
+elif choice == "4":
+    print("Decrypted:", decrypt_zigzag_route(text, cols))
+
+elif choice == "5":
+    print("Encrypted:", book_cipher_encrypt(text, book_text))
+
+elif choice == "6":
+    print("Decrypted:", book_cipher_decrypt(text, book_text))
