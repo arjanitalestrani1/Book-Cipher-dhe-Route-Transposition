3ba7617187493c88a4b114d96deca684c48b239e
arjanitalestrani1 <arjanita.lestrani@student.uni-pr.edu>
2026-04-19 14:21:47 +0200

    Implement column route cipher decryption

--- route_transposition_cipher.py
+++ route_transposition_cipher.py
@@ -27,6 +27,23 @@ def encrypt_column_route(text, cols):
     return result
 
 
+def decrypt_column_route(cipher, cols):
+    rows = math.ceil(len(cipher) / cols)
+    grid = [['' for _ in range(cols)] for _ in range(rows)]
+
+    k = 0
+    for j in range(cols):
+        for i in range(rows):
+            grid[i][j] = cipher[k]
+            k += 1
+
+    result = ""
+    for i in range(rows):
+        for j in range(cols):
+            result += grid[i][j]
+
+    return result.strip('X')
+
 # ========================================
 #    ZIG-ZAG ROUTE TRANSPOSITION CIPHER
 # ========================================
